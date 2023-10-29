88217b68f62e705e4f274db3b720207058ed025f
dhananjay0610 <dhananjaywaghade06@gmail.com>
2023-10-29 11:24:22 +0530

    added memoization

--- src/hooks/useMovieTrailer.js
+++ src/hooks/useMovieTrailer.js
@@ -1,11 +1,12 @@
 import { useEffect } from "react";
 import { API_OPTIONS } from "../utils/constants";
 
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addTrailerVideo } from "../utils/movieSlice";
 const useMovieTrailer = (movieId) => {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
+  const trailer = useSelector((store) => store.movies.trailer)
 
   const getMovieVideos = async () => {
     const data = await fetch(
@@ -16,12 +17,12 @@ const useMovieTrailer = (movieId) => {
 
     const filterData = json.results.filter((video) => video.type === "Trailer");
     const trailer = filterData.length ? filterData[0] : json.results[0];
-    // console.log(trailer);
+
     dispatch(addTrailerVideo(trailer));
   };
 
   useEffect(() => {
-    getMovieVideos();
+    !trailer && getMovieVideos();
   }, []);
 };
 
--- src/hooks/useNowPlayingMovies.js
+++ src/hooks/useNowPlayingMovies.js
@@ -1,23 +1,27 @@
-import  { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { API_OPTIONS } from "../utils/constants";
 import { addNowPlayingMovies } from "../utils/movieSlice";
 
 const useNowPlayingMovies = () => {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
-    const getNowPlayingMovie = async () => {
+  const nowPlayingMovies = useSelector(
+    (store) => store.movies.nowPlayingMovies
+  );
+
+  const getNowPlayingMovie = async () => {
     const data = await fetch(
       "https://api.themoviedb.org/3/movie/now_playing?page=1",
       API_OPTIONS
     );
     const json = await data.json();
-   // console.log(json?.results);
+    // console.log(json?.results);
     dispatch(addNowPlayingMovies(json.results));
   };
 
   useEffect(() => {
-    getNowPlayingMovie();
+    !nowPlayingMovies && getNowPlayingMovie();
   }, []);
 };
 
